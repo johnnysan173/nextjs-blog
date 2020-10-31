58a8ea87816e307eb9dde80acfd426b75aa7acc3
Johnny <johnnysan173@gmail.com>
2020-10-31 03:40:39 +0000

    change title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'ジョニー'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Johnny Blog by Next.js'
 
 export default function Layout({ children, home }) {
   return (

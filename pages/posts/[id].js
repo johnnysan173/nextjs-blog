aee8d83ac9d21a3f76081b6ba42add11f0b043df
Johnny <johnnysan173@gmail.com>
2020-11-14 02:28:43 +0000

    try SEO, clear unused format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,13 +5,18 @@ import Date from '../../components/date'
 import utilsStyles from '../../styles/utils.module.css'
 
 export default function Post({postData}) {
+    const thumblink = "https://johnnysan.info" + postData.thumb;
+    // console.log(thumblink);
     return (
         <Layout>
             <Head>
                 <title>{postData.title}</title>
+                <meta property="og:image" content={thumblink} />
+                <meta name="og:title" content={postData.title} />
+                <meta name="twitter:card" content="summary_large_image" />
             </Head>
             <article>
-                <h1 className={utilsStyles.headingXl}>{postData.title}</h1>
+                <h1>{postData.title}</h1>
                 <div className={utilsStyles.lightText}>
                     <Date dateString={postData.date} />
                 </div>
